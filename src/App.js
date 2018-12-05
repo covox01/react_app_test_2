15f905386a863c3fe2e49d3a2c74d50d05a256d5
Dennis Oba <oba.dennis@gmail.com>
2018-12-04 20:18:54 -0500

    git react test 1

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Test Now!
+            Learn React Test Now Please!
           </a>
         </header>
       </div>
